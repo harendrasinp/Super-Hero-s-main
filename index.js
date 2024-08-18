0d51af3e31cceb9f8a56dbea0478216226a5c3dc
harendrasinhp <haren111990@gmail.com>
2024-08-17 22:31:33 -0700

    view btn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const userInput = document.querySelector("#user-input");
 const btn = document.querySelector("#btn");
 const imgesection = document.querySelector("#images");
-const dtLoading= document.querySelector("#load");
+const dtLoading = document.querySelector("#load");
 
-dtLoading.style.display="none";
+dtLoading.style.display = "none";
 // -----------------------timestamp-------------------------------------
 let ts = Date.now().toString();
 // ------------------------marvel keys-----------------------------------
@@ -12,7 +12,7 @@ let publicKey = "e40adcdc6765246a7a4bd76aa8be2b03";
 // --------------------------md5 digest value-----------------------------
 let hashvalue = CryptoJS.MD5(ts + privateKey + publicKey).toString();
 // -----------------------------------------debounce-function---------------------------------
-function debounce(func,tm,fn) {
+function debounce(func, tm, fn) {
   let timeOutId;
   return function (...args) {
     fn();
@@ -24,22 +24,22 @@ function debounce(func,tm,fn) {
   };
 };
 async function callServer() {
-  imgesection.textContent=``;
+  imgesection.textContent = ``;
   const inputData = userInput.value;
   const heroName = await fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${inputData}&ts=${ts}&apikey=${publicKey}&hash=${hashvalue}`)
   if (heroName.ok == false) {
     console.log("Sorry");
-    dtLoading.style.display="none";
+    dtLoading.style.display = "none";
   };
   let jsonName = await heroName.json();
   heros = jsonName["data"]["results"];
   loopfunction(heros);
-  dtLoading.style.display="none";
+  dtLoading.style.display = "none";
 };
-userInput.addEventListener("input",debounce(callServer,500,load));
+userInput.addEventListener("input", debounce(callServer, 500, load));
 
-function load(){
-    dtLoading.style.display="block";
+function load() {
+  dtLoading.style.display = "block";
 }
 // --------------------------------function which adds proper data to proper place------------------------------------------------
 function loopfunction(heros) {
@@ -47,21 +47,22 @@ function loopfunction(heros) {
     const mainFram = document.createElement("div");
     mainFram.classList.add("main-fram");
 
-    mainFram.innerHTML = `<img src="${element["thumbnail"].path}.${element["thumbnail"].extension}" alt="" id="img-fram">
-                                            <div id="detail-div">
-                                              <div class="h-id">Id: ${element["id"]}</div>
-                                              <div class="h-name">Name: ${nameSize(element["name"])}</div>
-                                              <div class="h-comics">Comics: ${element["comics"].available}</div>
-                                            </div>
-                                                <div id="btn-div">
-                                                     <button type="submit" class="add-btn" heroPass=${ element["id"]}>Add to Favourite</button>
-                                                </div>`
+    mainFram.innerHTML = `<img src="${element["thumbnail"].path}.${element["thumbnail"].extension}"id="img-fram">
+                          <div id="detail-div">
+                            <div class="h-id">Id: ${element["id"]}</div>
+                            <div class="h-name">Name: ${nameSize(element["name"])}</div>
+                            <div class="h-comics">Comics: ${element["comics"].available}</div>
+                          </div>
+                          <div id="btn-div">
+                            <button type="submit" class="add-btn" id="detail-view" heroPass=${element["id"]}> View Detail</button>
+                            <button type="submit" class="add-btn" id="add-fav" heroPass=${element["id"]}>Add to Favourite</button>
+                          </div>`                  
     imgesection.appendChild(mainFram);
-    const hBtn = mainFram.querySelector(".add-btn");
+    const hBtn = mainFram.querySelector("#add-fav");
     hBtn.addEventListener("click", () => {
-      hBtn.style.backgroundColor="black";
-      hBtn.style.border="1px solid red"
-      hBtn.style.color="red";
+      hBtn.style.backgroundColor = "black";
+      hBtn.style.border = "1px solid red"
+      hBtn.style.color = "red";
       btnFunnction(hBtn);
       hBtn.innerHTML = "Added Succesfully"
     });
